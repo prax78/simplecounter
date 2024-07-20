7e005a2795026b610b76919accc7779bf36274e3
prax78 <prax78@gmail.com>
2024-07-20 22:43:18 +0530

    initial version

---
+++ counter.js
@@ -0,0 +1,28 @@
+
+    function about_me(){
+        window.open("https://prakash78blog.wordpress.com/");
+     
+    }
+
+    function add_step(){
+
+        let step=Number(document.getElementById("step").textContent);
+        document.getElementById("step").textContent=step+1;
+    }
+    
+     function refresh(){
+        let result=Number(document.getElementById("step").textContent);
+        document.getElementById("step").textContent=0;
+     
+        last_load(result);
+        if(localStorage.getItem("l") != null){
+            document.getElementById("last").textContent=`Last ${localStorage.getItem("l")}`; 
+        }
+     }
+
+     function last_load(data){
+        if(data !=0){
+            localStorage.setItem("l",`${data}`);
+        }
+     
+     }
